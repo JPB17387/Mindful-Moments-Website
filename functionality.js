e5956be8971d2df67c231f0fae59242de92571bc
Jhon Paul Baonil <paulbaonil@gmail.com>
2025-07-02 12:44:21 -0700

    first commit

---
+++ functionality.js
@@ -0,0 +1,11 @@
+function showMessage(mood) {
+  const messages = {
+    happy: "Keep smiling and spread that good energy!",
+    sad: "It’s okay to feel sad. You're not alone.",
+    anxious: "Take deep breaths. You are doing your best.",
+    tired: "Rest is also productive. Be kind to yourself.",
+    motivated: "Great! Use your energy to help someone today!"
+  };
+  
+  document.getElementById("message").textContent = messages[mood];
+}
